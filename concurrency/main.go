fe71fdae54798e7c06b96e68cec73914c49397bb
ikaz <icastillejos@gmail.com>
2021-09-21 21:11:45 -0700

    Adding deps

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
 
 func main() {
+	t := tree.New(1)
 	ch := make(chan int)
 	close(ch)
 	for v := range ch {
 		fmt.Println(v)
 	}
-
+	fmt.Printf("Tree: %+v", t)
 	fmt.Printf("TODO... %+v", ch)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ikaz/edio.gointro
 
 go 1.17
+
+require golang.org/x/tour v0.1.0
